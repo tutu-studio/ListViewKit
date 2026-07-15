5d8ca6de521da1c561ede0771a0e0ee638d19949
Tualatrix Chou <tualatrix@gmail.com>
2026-07-15 12:36:30 +0800

    refactor: remove unchecked reference sendability
    - stop declaring mutable generic references as universally Sendable
    - rely on the owning UI types' main actor isolation
    - remove unused identity and hashing conformances from the internal wrapper

--- Sources/Reference.swift
+++ Sources/Reference.swift
@@ -3,10 +3,8 @@
 //  Copyright (c) 2025 ktiays. All rights reserved.
 //
 
-import Foundation
-
 @propertyWrapper
-final class Reference<T>: @unchecked Sendable {
+final class Reference<T> {
     var wrappedValue: T
 
     init(wrappedValue: T) {
@@ -19,23 +17,5 @@ final class Reference<T>: @unchecked Sendable {
 
     func modifying<R>(_ modifier: (inout T) throws -> R) rethrows -> R {
         try modifier(&wrappedValue)
-    }
-}
-
-extension Reference: Identifiable where T: Identifiable {
-    var id: T.ID {
-        wrappedValue.id
-    }
-}
-
-extension Reference: Equatable where T: Equatable {
-    static func == (lhs: Reference<T>, rhs: Reference<T>) -> Bool {
-        lhs.wrappedValue == rhs.wrappedValue
-    }
-}
-
-extension Reference: Hashable where T: Hashable {
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(wrappedValue)
     }
 }
